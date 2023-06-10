efb84f010d83d1bfb3810ed60d05c7295b0733f3
jg <jg@gore.cc>
2023-06-10 17:21:23 -0500

    removed github provider, added remote tf state in Azure

--- main.tf
+++ main.tf
@@ -1,30 +1,29 @@
 resource "random_pet" "project_name" {
   keepers = {
-  name = var.project_name
-}
-}
-
-resource "azuredevops_project" "test-proj" {
-	name = "gorecc-${random_pet.project_name.id}"
-	description = "A test project"
-	visibility = "private"
-	version_control = "Git"
-	work_item_template = "Agile"
+    name = var.project_name
+  }
 }
 
-resource "azuredevops_git_repository" "test-repo" {
-	project_id = azuredevops_project.test-proj.id
-	name = "Example Git Repo"
-	default_branch = "refs/heads/main"
-	initialization {
-		init_type = "Clean"
-	}
-	}
-
-data "github_user" "current" {
-	username = "jlgore"
+variable "project_name" {
+  type    = string
+  default = "tf"
 }
 
-output "ssh_keys" {
-	value = data.github_user.current.ssh_keys
+resource "azuredevops_project" "proj" {
+  name               = "gorecc-${random_pet.project_name.id}"
+  description        = "A test project"
+  visibility         = "private"
+  version_control    = "Git"
+  work_item_template = "Agile"
 }
+
+resource "azuredevops_git_repository" "repo" {
+  project_id     = azuredevops_project.proj.id
+  name           = "Example Git Repo"
+  default_branch = "refs/heads/main"
+  initialization {
+    init_type = "Clean"
+  }
+}
+
+
--- provider.tf
+++ provider.tf
@@ -1,46 +1,40 @@
 terraform {
   required_providers {
     azuredevops = {
-      source = "microsoft/azuredevops"
+      source  = "microsoft/azuredevops"
       version = "0.5.0"
     }
-    github = {
-    source = "integrations/github"
-    version = "5.26.0"
-  }
+    #    github = {
+    #    source = "integrations/github"
+    #    version = "5.26.0"
+    #  }
     random = {
-    source = "hashicorp/random"
-    version = "3.5.1"
-}
-}
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstategorecc"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 variable "org_service_url" {
-	type = string
+  type = string
 }
 
 variable "personal_access_token" {
-	type = string
-}
-
-variable "project_name" {
   type = string
 }
 
 provider "azuredevops" {
- 
- org_service_url = var.org_service_url
- personal_access_token = var.personal_access_token
+
+  org_service_url       = var.org_service_url
+  personal_access_token = var.personal_access_token
 
 }
-
-variable "gh_personal_access_token" {
-	type = string
-}
-
-provider "github" {
-  token = var.gh_personal_access_token
-  }
 
 provider "random" {
 
